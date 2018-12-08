7cd5ca72c62f6c52f8665c87d69b0e8e5bb28c3a
Alejandra Paz <pdinaalejandra@gmail.com>
2018-12-07 20:34:30 -0600

    Subiendo proyecto completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
-
+import {Empleado} from './models/empleado';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crud-simple';
+
+  empleadoArray:Empleado[]=[
+  {id:1,nombre:"Ale",pais:"Honduras"},
+  {id:2,nombre:"Mita",pais:"USA"},
+  {id:3,nombre:"Dina",pais:"Francia"},
+  {id:4,nombre:"Alejandra",pais:"Costa Rica"}
+  ];
+
+   selectEmpleado: Empleado=new Empleado();
+  
+  addOrEdit(){
+  	if(this.selectEmpleado.id===0){
+  		this.selectEmpleado.id= this.empleadoArray.length+1;
+  	    this.empleadoArray.push(this.selectEmpleado);
+
+  	}
+  	this.selectEmpleado=new Empleado ();
+
+  }
+
+  openForEdit(empleado: Empleado){
+  	this.selectEmpleado=empleado;
+
+  }
+
+   delete(){
+   	if(confirm('Estas seguro de querer eliminarlo?')){
+   		this.empleadoArray=this.empleadoArray.filter(x=>x!= this.selectEmpleado);
+  	this.selectEmpleado=new Empleado();
+   	}
+  	
+  }
+
+
 }
